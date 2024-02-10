ad03650d8481f5d47bcad55fc464f0042d832fad
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-02-10 16:58:27 -0500

    Refactor RandomFox component to lazy load image

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,4 +1,5 @@
 import type { FC, FunctionComponent } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // export const RandomFox = () => { IMPLICIT TYPESCRIPT
 //     return <img />
@@ -17,7 +18,35 @@ type Props = {
   alt: string;
 };
 export const RandomFox = ({ image, alt }: Props): JSX.Element => {
+  const ref = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
   return (
-    <img src={image} width={320} height="auto" className="rounded" alt={alt} />
+    <img
+      ref={ref}
+      src={src}
+      width={320}
+      height="auto"
+      className="rounded bg-gray-300"
+      alt={alt}
+    />
   );
 };
